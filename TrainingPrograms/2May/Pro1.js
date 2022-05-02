edfa27599e1f77cce8307d09d72385e4a0b2cd39
gourav <gouravkumardodia@gmail.com>
2022-05-02 10:24:04 +0530

    2may

---
+++ TrainingPrograms/2May/Pro1.js
@@ -0,0 +1,10 @@
+var num = 100;
+var cname = "Wipro Training";
+var sstatus = true;
+var data = 34;
+
+console.log(num);
+console.log("Num : ", num);
+console.log(cname);
+console.log(sstatus);
+console.log(data);
---
+++ TrainingPrograms/2May/Pro1.ts
@@ -0,0 +1,12 @@
+var num:number = 100;
+var cname:string = "Wipro Training";
+var sstatus:boolean = true;
+var data:any = 34;
+
+
+console.log(num)
+console.log("Num : ", num)
+
+console.log(cname)
+console.log(sstatus)
+console.log(data)
