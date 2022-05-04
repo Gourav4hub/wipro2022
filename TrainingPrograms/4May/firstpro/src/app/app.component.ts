dac07e1c6cb97345c4dc036c9fbd59e3148d6d4f
gourav <gouravkumardodia@gmail.com>
2022-05-04 12:46:32 +0530

    4may

--- TrainingPrograms/4May/firstpro/src/app/app.component.ts
+++ TrainingPrograms/4May/firstpro/src/app/app.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
 
 export class AppComponent 
 {
-  title = 'firstpro';
+  public title:string = "Wipro Full Stack Training !"
+  public num:number = 100;
+  public student:any = {
+    roll : 101,
+    name : 'vikas',
+    marks : 345.45
+  }
+
+  public handleChangeTitle():void
+  {
+    this.title = "My Name is Gourav Kumar !"
+  }
+
+  public handleChangeNum():void
+  {
+    this.num += 10;
+  }
 }
