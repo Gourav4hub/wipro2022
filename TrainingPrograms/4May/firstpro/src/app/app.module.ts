4c3fcf7f3e703a37a57675169a2181abbe02074e
gourav <gouravkumardodia@gmail.com>
2022-05-04 11:40:02 +0530

    4may

--- TrainingPrograms/4May/firstpro/src/app/app.module.ts
+++ TrainingPrograms/4May/firstpro/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
