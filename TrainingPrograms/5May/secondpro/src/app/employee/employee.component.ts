e31962a82b13cf3c5cd7cfcfb0130f46478a49e8
gourav <gouravkumardodia@gmail.com>
2022-05-05 12:45:58 +0530

    5may

--- TrainingPrograms/5May/secondpro/src/app/employee/employee.component.ts
+++ TrainingPrograms/5May/secondpro/src/app/employee/employee.component.ts
@@ -14,12 +14,36 @@ export class EmployeeComponent implements OnInit
 {
   public employees:any = []
 
+  public deleteInfo:any = {
+      isdelete : false,
+      employee : undefined
+  }
+
   constructor() { 
       //this.employees = data
       this.employees = jsonData
   }
 
   ngOnInit(): void {
+  }
+
+  public deleteEmployee(emp:any)
+  {
+    this.deleteInfo = {
+      isdelete : true,
+      employee : emp
+    }
+  }
+  public cancelDelete():void{
+    this.deleteInfo = {
+      isdelete : false,
+      employee : undefined
+    }
+  }
+  public delete():void
+  {
+     this.employees = this.employees.filter((ob:any)=>ob.empid!=this.deleteInfo.employee.empid)
+     this.cancelDelete()
   }
 
   public saveEmployee(empid:any,empname:string,department:string,salary:any):void
