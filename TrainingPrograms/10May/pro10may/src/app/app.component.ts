a35bf53b595a04bda36960a9d1149499b9caf9c0
gourav <gouravkumardodia@gmail.com>
2022-05-10 10:51:26 +0530

    10may

--- TrainingPrograms/10May/pro10may/src/app/app.component.ts
+++ TrainingPrograms/10May/pro10may/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent
 {
    public cities:any = ['Pune','Delhi','Raipur','Mumbai','Indore']
+
+  public saveEmployee(data:any):void
+  {
+    console.log(data)
+  }
 }
