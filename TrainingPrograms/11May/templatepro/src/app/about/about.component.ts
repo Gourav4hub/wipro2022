bb7d583f91cfddf5820c47872254634d22a513ee
gourav <gouravkumardodia@gmail.com>
2022-05-11 12:55:31 +0530

    11may

--- TrainingPrograms/11May/templatepro/src/app/about/about.component.ts
+++ TrainingPrograms/11May/templatepro/src/app/about/about.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
+export class AboutComponent implements OnInit 
+{
+  public users:any = []
+  constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
   }
 
+  public load():void
+  {
+    var ob:Observable<any> = this.http.get("https://jsonplaceholder.typicode.com/users")
+
+    ob.subscribe((data:any)=>{
+        this.users = data
+    })
+
+  }
 }
--- TrainingPrograms/11May/templatepro/src/app/app.module.ts
+++ TrainingPrograms/11May/templatepro/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -20,7 +22,8 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
