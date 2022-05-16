870006433f7c92d1d66638ed3949605135938e9b
gourav <gouravkumardodia@gmail.com>
2022-05-16 12:26:12 +0530

    16May

--- TrainingPrograms/16May/web16may/src/app/app.component.ts
+++ TrainingPrograms/16May/web16may/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup , FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web16may';
+export class AppComponent 
+{
+  public userFrmGroup:FormGroup;
+  constructor(){
+    this.userFrmGroup = new FormGroup({
+        basicInfo : new FormGroup({
+          userName : new FormControl(null,Validators.required),
+          userEmail : new FormControl(null,[Validators.required,Validators.email]),
+          userPhone : new FormControl(null,Validators.required),
+          userPassword : new FormControl(null,Validators.required)
+        }),
+        otherInfo : new FormGroup({
+          userDOB : new FormControl()
+        })       
+    })   
+  }
+
+  public register():void{
+    console.log(this.userFrmGroup.value)
+  }
 }
