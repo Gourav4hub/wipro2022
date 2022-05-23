45a4341b61a060b5a782762098a0911cef182651
gourav <gouravkumardodia@gmail.com>
2022-05-23 12:58:08 +0530

    23May

---
+++ TrainingPrograms/wiproworkspace/InheritanceDemo/src/model3/Faculty.java
@@ -0,0 +1,37 @@
+package model3;
+
+import java.util.Scanner;
+
+public class Faculty extends Person 
+{
+	private int facid;
+	private float salary;
+	
+	public Faculty() {	
+	}
+	
+	public Faculty(int facid, float salary) {
+		super();
+		this.facid = facid;
+		this.salary = salary;
+	}
+	
+//	public void canVote() 
+//	{}
+
+	public void input() 
+	{
+		Scanner sc = new Scanner(System.in);
+		super.input();
+		System.out.print("Facid : "); this.facid = sc.nextInt();
+		System.out.print("Salary : "); this.salary = sc.nextFloat();
+	}	
+	
+	public void show() 
+	{
+		super.show();
+		System.out.println("FacId : " + this.facid);
+		System.out.println("Salary : " + this.salary);
+	}
+	
+}
---
+++ TrainingPrograms/wiproworkspace/InheritanceDemo/src/model3/Pro1.java
@@ -0,0 +1,17 @@
+package model3;
+
+public class Pro1 {
+
+	public static void main(String[] args) 
+	{
+		// Faculty p1 = new Faculty();
+		
+		Person p1 = new Person(); // 
+				
+		p1.input();				
+		p1.show();
+		p1.canVote();	
+		
+	}
+
+}
