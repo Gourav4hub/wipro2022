3ac6c61f93f392865541927179c062c9b2f54880
gourav <gouravkumardodia@gmail.com>
2022-06-01 11:36:01 +0530

    1june

--- TrainingPrograms/SpringSpace/Web1Jun/src/main/java/com/wipro/boot/controller/WebController.java
+++ TrainingPrograms/SpringSpace/Web1Jun/src/main/java/com/wipro/boot/controller/WebController.java
@@ -1,14 +1,42 @@
 package com.wipro.boot.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController 
 {
-	@RequestMapping(value = "/test")
+	@RequestMapping(value = "/test1")
 	public String fun1() 
 	{
-		return "Good Morning !";
+		return "Good Morning Everyone !";
+	}	
+	
+	// localhost:8081/test2?name=lokesh
+	// localhost:8081/test2?name=Vikas&age=45
+	@RequestMapping(value = "/test2") // Receive a String name
+	public String fun2(@RequestParam(value = "name") String userName,
+				@RequestParam(value = "age") int age) 
+	{
+		return "Hello " + userName + " : " + age;
+	}
+	
+	@RequestMapping(value = "/test3") 
+	public String fun3(@RequestParam(value = "name") String userName,
+				@RequestParam(value = "age",required = false, defaultValue = "0") int age) 
+	{
+		return "Hello " + userName + " : " + age;
+	}
+	
+	@RequestMapping(value = "/test4") 
+	public String fun4(@RequestParam(value = "name") String userName,
+				@RequestParam(value = "age",required = false) Integer age) 
+	{
+		if(age==null)
+			return "Hello " + userName;
+		else
+			return "Hello " + userName + " : " + age;		
 	}
 }
