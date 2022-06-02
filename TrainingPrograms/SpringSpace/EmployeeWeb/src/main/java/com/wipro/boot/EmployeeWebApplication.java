3518ba6373d39439bf9d51176eb1d927b9fe522f
gourav <gouravkumardodia@gmail.com>
2022-06-02 09:23:46 +0530

    2Jun

---
+++ TrainingPrograms/SpringSpace/EmployeeWeb/src/main/java/com/wipro/boot/EmployeeWebApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWebApplication.class, args);
+	}
+
+}
