724d266c8062d26171639caa788e03bc235f4373
gourav <gouravkumardodia@gmail.com>
2022-06-20 12:25:26 +0530

    20june

---
+++ Training17june/officeapp/src/main/java/com/wipro/officeapp/model/Employee.java
@@ -0,0 +1,38 @@
+package com.wipro.officeapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "emp_info")
+@Data
+public class Employee 
+{
+	@Id
+	@Column(name = "emp_id")
+	private String empId;
+	
+	@Column(name = "emp_name",nullable = false)
+	private String empName;
+	
+	@Column(name = "emp_salary" , nullable = false)
+	private Float salary;
+	
+	@Column(name = "emp_city", nullable = true)
+	private String city;
+	
+	@Column(name = "emp_phone",nullable = true,unique = true)
+	private String phone;	
+}
+
+
+
+
+
+
+
+
