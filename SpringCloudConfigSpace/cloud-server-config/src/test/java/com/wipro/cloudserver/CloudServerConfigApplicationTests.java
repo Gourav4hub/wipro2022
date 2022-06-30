238010cde455bf94ea71cf07289aaa81b741b55a
gourav <gouravkumardodia@gmail.com>
2022-06-30 12:02:46 +0530

    30Jun

---
+++ SpringCloudConfigSpace/cloud-server-config/src/test/java/com/wipro/cloudserver/CloudServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.cloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
